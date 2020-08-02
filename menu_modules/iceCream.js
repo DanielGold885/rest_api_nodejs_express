607b16a6b34413a2ecf377c1df0e6509f4ba663e
Daniel Gold <danielg@jfrog.com>
2020-08-02 08:54:05 +0300

    more code

--- menu_modules/iceCream.js
+++ menu_modules/iceCream.js
@@ -3,7 +3,6 @@ const DATA_FILE = './menu_data/icecream.json';
 
 
 let iceCream = {
-    //Make get a function and pass in in 2 callbacks (Resolve & Reject)
     get: (resolve, reject) => {
         fs.readFile(DATA_FILE, (error, data) => {
             if (error) {
@@ -18,3 +17,4 @@ let iceCream = {
 
 
 module.exports = iceCream;
+
